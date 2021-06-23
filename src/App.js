62dab5c12b82815443aad53b012ac035d1f3cb50
Femi Akinyemi <santosfemakin@yahoo.com>
2021-06-23 14:45:03 +0100

    Testing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           Thank God it now worked
         </p>
+        <p>React Master TESTING</p>
         <a
           className="App-link"
           href="https://reactjs.org"
