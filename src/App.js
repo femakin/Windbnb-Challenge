22eae2dd324a293d030ed9a1eba6d59754d1d313
Femi Akinyemi <santosfemakin@yahoo.com>
2021-06-23 14:36:45 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Thank God it now worked
         </p>
         <a
           className="App-link"
